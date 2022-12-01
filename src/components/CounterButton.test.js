da3cab0dc42481b63e96ea396ab11683b22974c5
DawidWebb <tomasz.matras@developerweb.pl>
2022-12-01 21:45:20 +0100

    add test for Card, CardList and CounterButton

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CounterButton from './CounterButton';
+import React from "react";
+import { shallow } from "enzyme";
+import CounterButton from "./CounterButton";
 
-it('renders without crashing', () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+it("expoect to render CounterButton component", () => {
+	const mockColor = `red`;
+
+	expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
 });
 
-it('correnctly increments the counter', () => {
-  const wrapper = shallow(<CounterButton/>)
-  expect(wrapper).toMatchSnapshot();
-  wrapper.find('[id="counter"]').simulate('click');
-  expect((wrapper.state())).toEqual({count: 1})
-  wrapper.find('[id="counter"]').simulate('click');
-  wrapper.find('[id="counter"]').simulate('click');
-  expect((wrapper.state())).toEqual({count: 3})
+it("correctly increments the count", () => {
+	const mockColor = `red`;
+	const wrapper = shallow(<CounterButton color={mockColor} />);
+
+	wrapper.find('[id="counter"]').simulate("click");
+	expect(wrapper.state()).toEqual({ count: 1 });
+	wrapper.find('[id="counter"]').simulate("click");
+	wrapper.find('[id="counter"]').simulate("click");
+	expect(wrapper.state()).toEqual({ count: 3 });
 });
